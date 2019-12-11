2d2f1e03213ffcfa1123bf81ba76f3ee8d5e09d1
renanboni <renan.boni@usp.br>
2019-12-10 22:36:45 -0300

    Created ActorBeta base class

--- desktop/src/com/boni/libgdx/desktop/DesktopLauncher.java
+++ desktop/src/com/boni/libgdx/desktop/DesktopLauncher.java
@@ -2,9 +2,7 @@ package com.boni.libgdx.desktop;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.boni.libgdx.StarfishCollectorAlpha;
-import com.boni.libgdx.libgdx;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
