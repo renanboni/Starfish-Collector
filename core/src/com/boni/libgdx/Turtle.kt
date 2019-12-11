59f077e2ca723f6a74f61ca754789a0cc2ca9b36
renanboni <renan.boni@usp.br>
2019-12-11 19:38:56 -0300

    Added movements to the turtle

--- core/src/com/boni/libgdx/Turtle.kt
+++ core/src/com/boni/libgdx/Turtle.kt
@@ -1,18 +1,40 @@
 package com.boni.libgdx
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.scenes.scene2d.Stage
 
-class Turtle (x: Float, y: Float, stage: Stage): BaseActor(x, y, stage) {
+class Turtle(x: Float, y: Float, stage: Stage) : BaseActor(x, y, stage) {
 
     init {
-        val filenames = Array<String>(6) {
-            "turtle-1.png"
-            "turtle-2.png"
-            "turtle-3.png"
-            "turtle-4.png"
-            "turtle-5.png"
-            "turtle-6.png"}
+        val filenames = arrayOf("turtle-1.png",
+                "turtle-2.png",
+                "turtle-3.png",
+                "turtle-4.png",
+                "turtle-5.png",
+                "turtle-6.png")
+
+        setAcceleration(400f)
+        setMaxSpeed(100f)
+        setDeceleration(400f)
 
         loadAnimationFromFiles(filenames, 0.1f, true)
+    }
+
+    override fun act(delta: Float) {
+        super.act(delta)
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) accelerateAtAngle(180f)
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) accelerateAtAngle(0f)
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) accelerateAtAngle(90f)
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) accelerateAtAngle(270f)
+
+        applyPhysics(delta)
+
+        setAnimationPaused(!isMoving())
+
+        if (getSpeed() > 0) {
+            rotation = getMotionAngle()
+        }
     }
 }
