7962412c534d97d0225805549a79b9edb4e13104
renanboni <renan.boni@usp.br>
2019-12-16 23:03:38 -0300

    Removed wildcard imports from project

--- core/src/com/boni/libgdx/BaseActor.kt
+++ core/src/com/boni/libgdx/BaseActor.kt
@@ -5,7 +5,11 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
-import com.badlogic.gdx.math.*
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Polygon
+import com.badlogic.gdx.math.Intersector
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.scenes.scene2d.Group
 import com.badlogic.gdx.scenes.scene2d.Stage
 
