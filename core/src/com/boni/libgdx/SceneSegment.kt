abff28538f081c046c90e304c7e11c59a82c8491
renanboni <renan.boni@usp.br>
2019-12-17 23:24:20 -0300

    Created the SceneSegment but need to populate it (kinda drunk right now..)

---
+++ core/src/com/boni/libgdx/SceneSegment.kt
@@ -0,0 +1,9 @@
+package com.boni.libgdx
+
+import com.badlogic.gdx.scenes.scene2d.Action
+import com.badlogic.gdx.scenes.scene2d.Actor
+
+class SceneSegment(private val actor: Actor, private val action: Action) {
+
+
+}
