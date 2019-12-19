244222138d5ae9c8383f023acf5691eee77d4618
Renan Boni <renan.boni.ext@neon.com.br>
2019-12-19 19:55:09 -0300

    Finishing SceneSegment

--- core/src/com/boni/libgdx/SceneSegment.kt
+++ core/src/com/boni/libgdx/SceneSegment.kt
@@ -5,5 +5,18 @@ import com.badlogic.gdx.scenes.scene2d.Actor
 
 class SceneSegment(private val actor: Actor, private val action: Action) {
 
+    fun start() {
+        actor.clearActions()
+        actor.addAction(action)
+    }
 
+    fun finish() {
+        if (actor.hasActions()) {
+            actor.actions.first().act(100000f)
+        }
+
+        actor.clearActions()
+    }
+
+    fun isFinished() = actor.actions.isEmpty
 }
